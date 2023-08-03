b04acce47f5c20c80c686ca274790c9e786ae5c2
syedaromi <141348046+syedaromi@users.noreply.github.com>
2023-08-03 20:45:48 +0500

    My First Python Assignment

---
+++ Assignment 1 Python.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ##### Printing Hello World Using a Variable
+
+# In[2]:
+
+
+message = "Hello World"
+print(message)
+
+
+# ##### Printing Hello World Using a List
+
+# In[4]:
+
+
+mylist = ["Hello","World"]
+print(mylist) 
+
+
+# ##### Printing Hello World Using a Sets
+
+# In[10]:
+
+
+myset = {"Hello", "World", "Hello", "World"}
+
+print(myset)
+
+
+# ##### Printing Hello World Using  Data Type (Str)
+
+# In[11]:
+
+
+x = "Hello World"
+
+print(x)
+
+print(type(x)) 
+
+
+# ##### Concatenate two Python Strings to Print Hello World
+
+# In[2]:
+
+
+word1 = "Hello"
+word2 = "World"
+print(word1 + " " + word2)
+
